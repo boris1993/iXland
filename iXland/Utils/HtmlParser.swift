2b86fd5549b5ef2470e03faa4690a97a51f61ce8
Boris Zhao <boris1993@live.cn>
2023-11-19 22:12:19 +0800

    refactor: Refactoring HtmlParser

--- iXland/Utils/HtmlParser.swift
+++ iXland/Utils/HtmlParser.swift
@@ -4,17 +4,15 @@ import SwiftSoup
 class HtmlParser {
     private init() {}
 
-    private static let XPathTextNodeAnywhere = "//text()"
-    private static let XPathBRNode = "br"
-    private static let XPathBRNodeEverywhere = "//br"
+    private static let NewLine = "\n"
 
     public static func normalizeTexts(content: String) -> String {
         do {
             let document = try SwiftSoup.parse(content.replacingOccurrences(of: "\r\n", with: ""))
             document.outputSettings(OutputSettings().prettyPrint(pretty: false))
 
-            replaceBrToLineBreak(document: document)
-            normalizeLinks(document: document)
+            try replaceBrToLineBreak(document: document)
+            try normalizeLinks(document: document)
 
             return try document.text(trimAndNormaliseWhitespace: false)
         } catch {
@@ -23,28 +21,20 @@ class HtmlParser {
         }
     }
 
-    private static func replaceBrToLineBreak(document: Document) {
-        do {
-            let brNodes = try document.select("br")
-            try brNodes.forEach { node in
-                try node.text("\n")
-            }
-        } catch {
-            print(error)
+    private static func replaceBrToLineBreak(document: Document) throws {
+        let brNodes = try document.select("br")
+        try brNodes.forEach { node in
+            try node.text(NewLine)
         }
     }
 
-    private static func normalizeLinks(document: Document) {
-        do {
-            let links = try document.select("a")
-            try links.forEach { link in
-                let linkHref = try link.attr("href")
-                let linkText = try link.text()
+    private static func normalizeLinks(document: Document) throws {
+        let links = try document.select("a")
+        try links.forEach { link in
+            let linkHref = try link.attr("href")
+            let linkText = try link.text()
 
-                try link.text("[\(linkText)](\(linkHref))")
-            }
-        } catch {
-            print(error)
+            try link.text("[\(linkText)](\(linkHref))")
         }
     }
 }
